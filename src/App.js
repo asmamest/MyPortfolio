4a0676ee91d6b65694853b0a0f0344f4c427134c
Mestaysser <mestaysserasma@gmail.com>
2024-08-24 17:35:54 +0100

    kjkjkj

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import { Analytics } from "@vercel/analytics/react"
+// import { Analytics } from "@vercel/analytics/react"
 import Sidebar from'./components/sidebar/Sidebar';
 import Home from './components/home/Home';
 import About from './components/about/About';
 import Portfolio from './components/portfolio/Portfolio';
-import Services from './components/services/Services';
+// import Services from './components/services/Services';
 import Resume from './components/resume/Resume';
-import Testimonials from './components/testimonials/Testimonials';
+// import Testimonials from './components/testimonials/Testimonials';
 import Blog from './components/blog/Blog';
 import Contact from './components/contact/Contact';
 
@@ -15,7 +15,7 @@ import Contact from './components/contact/Contact';
 const App = ()=> {
   return (
     <>
-      <Analytics/>
+      {/* <Analytics/> */}
       <Sidebar />
       <main className='main'>
         <Home/>
